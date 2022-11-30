1b3e3a7476ffca7e3ae903030eed2723113b39f9
bmatthieu3 <matthieu.baumann@astro.unistra.fr>
2022-11-30 14:03:58 +0100

    update doc and scripts to build a conda package

--- conda-recipe/post-link.sh
+++ conda-recipe/post-link.sh
@@ -1,5 +1,5 @@
 # Activate ipyaladin for simple notebooks
-"${PREFIX}/bin/jupyter-nbextension" --py --symlink --overwrite --sys-prefix ipyaladin
+"${PREFIX}/bin/jupyter-nbextension" enable --py widgetsnbextension
 "${PREFIX}/bin/jupyter-nbextension" enable --py --sys-prefix ipyaladin
-# Activate ipyaladin extension for jupyter lab
-"${PREFIX}/bin/jupyter-labextension" develop ipyaladin --overwrite
+# Activate ipyaladin extension for jupyter lab (todo, currently it does not work)
+# "${PREFIX}/bin/jupyter-labextension" develop ipyaladin --overwrite
