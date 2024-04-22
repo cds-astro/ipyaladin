cb99a4b5c8406d29952da185f176e4df13f5b33d
Xen0Xys <red57101@gmail.com>
2024-04-22 09:48:14 +0000

    :white_check_mark: Improve testing for new coordinate parsing functions

--- src/test/test_coordinate_parser.py
+++ src/test/test_coordinate_parser.py
@@ -3,9 +3,45 @@ from ipyaladin.coordinate_parser import (
     parse_coordinate_string,
     _split_coordinate_string,
     _is_hour_angle_string,
+    _is_coordinate_string,
 )
 from astropy.coordinates import SkyCoord
 import pytest
+
+test_is_coordinate_string_values = [
+    ("M 31", False),
+    ("sgr a*", False),
+    ("1:12:43.2 31:12:43", True),
+    ("1:12:43.2 +31:12:43", True),
+    ("1:12:43.2 -31:12:43", True),
+    ("1 12 43.2 31 12 43", True),
+    ("1 12 43.2 +31 12 43", True),
+    ("1 12 43.2 -31 12 43", True),
+    ("1h12m43.2s 1d12m43s", True),
+    ("1h12m43.2s +1d12m43s", True),
+    ("1h12m43.2s -1d12m43s", True),
+    ("42.67 25.48", True),
+    ("42.67 +25.48", True),
+    ("42.67 -25.48", True),
+    ("0 0", True),
+    ("J42.67 25.48", True),
+    ("G42.67 25.48", True),
+    ("B42.67 25.48", True),
+    ('17h 45m 40.0409s -29° 00′ 28.118"', True),  # noqa RUF001
+    ("17h 45m 40.0409s -29° 00' 28.118\"", True),
+]
+
+
+@pytest.mark.parametrize(("inp", "expected"), test_is_coordinate_string_values)
+def test_is_coordinate_string(inp, expected):
+    """
+    Test the function _is_coordinate_string
+    :param inp: A string with an object name or coordinates to check
+    :param expected: The expected result as a boolean
+    :return: None
+    """
+    assert _is_coordinate_string(inp) == expected
+
 
 test_split_coordinate_string_values = [
     ("1:12:43.2 31:12:43", ("1:12:43.2", "31:12:43")),
@@ -21,11 +57,20 @@ test_split_coordinate_string_values = [
     ("42.67 +25.48", ("42.67", "+25.48")),
     ("42.67 -25.48", ("42.67", "-25.48")),
     ("0 0", ("0", "0")),
+    ("J42.67 25.48", ("42.67", "25.48")),
+    ("G42.67 25.48", ("42.67", "25.48")),
+    ("B42.67 25.48", ("42.67", "25.48")),
 ]
 
 
 @pytest.mark.parametrize(("inp", "expected"), test_split_coordinate_string_values)
 def test_split_coordinate_string(inp, expected):
+    """
+    Test the function _split_coordinate_string
+    :param inp: A string with coordinates to split
+    :param expected: The expected result as a tuple of strings
+    :return: None
+    """
     assert _split_coordinate_string(inp) == expected
 
 
@@ -40,12 +85,20 @@ test_is_hour_angle_string_values = [
 
 @pytest.mark.parametrize(("inp", "expected"), test_is_hour_angle_string_values)
 def test_is_hour_angle_string(inp, expected):
+    """
+    Test the function _is_hour_angle_string
+    :param inp: A coordinate part as a string
+    :param expected: The expected result as a boolean
+    :return: None
+    """
     assert _is_hour_angle_string(inp) == expected
 
 
 test_parse_coordinate_string_values = [
     ("M 31", SkyCoord.from_name("M 31")),
     ("sgr a*", SkyCoord.from_name("sgr a*")),
+    ("α Centauri", SkyCoord.from_name("α Centauri")),  # noqa RUF001
+    ("* 17 Com", SkyCoord.from_name("* 17 Com")),
     (
         "1:12:43.2 31:12:43",
         SkyCoord(ra="1:12:43.2", dec="31:12:43", unit=("hour", "deg")),
@@ -86,17 +139,46 @@ test_parse_coordinate_string_values = [
     ("42.67 +25.48", SkyCoord(ra=42.67, dec=25.48, unit="deg")),
     ("42.67 -25.48", SkyCoord(ra=42.67, dec=-25.48, unit="deg")),
     ("0 0", SkyCoord(ra=0, dec=0, unit="deg")),
+    ("J42.67 25.48", SkyCoord(ra=42.67, dec=25.48, unit="deg")),
+    (
+        "G42.67 25.48",
+        SkyCoord(l=42.67, b=25.48, unit="deg", frame="galactic"),
+    ),
+    (
+        "B42.67 25.48",
+        SkyCoord(ra=42.67, dec=25.48, unit="deg", frame="fk4", equinox="B1950"),
+    ),
+    (
+        "J12 30 45 +45 30 15",
+        SkyCoord(ra="12 30 45", dec="45 30 15", unit=("hour", "deg")),
+    ),
+    (
+        "J03 15 20 -10 20 30",
+        SkyCoord(ra="03 15 20", dec="-10 20 30", unit=("hour", "deg")),
+    ),
+    ("G120.5 -45.7", SkyCoord(l=120.5, b=-45.7, unit="deg", frame="galactic")),
+    ("G90 0", SkyCoord(l=90, b=0, unit="deg", frame="galactic")),
+    ("B60 30", SkyCoord(ra=60, dec=30, unit="deg", frame="fk4", equinox="B1950")),
+    ("B120 -45", SkyCoord(ra=120, dec=-45, unit="deg", frame="fk4", equinox="B1950")),
 ]
 
 
 @pytest.mark.parametrize(("inp", "expected"), test_parse_coordinate_string_values)
 def test_parse_coordinate_string(inp, expected):
+    """
+    Test the function parse_coordinate_string
+    :param inp: The string to parse
+    :param expected: The expected result as a SkyCoord object
+    :return: None
+    """
     assert parse_coordinate_string(inp) == expected
 
 
 test_aladin_string_target = [
     "M 31",
     "sgr a*",
+    "α Centauri",  # noqa RUF001
+    "* 17 Com",
     "1:12:43.2 31:12:43",
     "1:12:43.2 +31:12:43",
     "1:12:43.2 -31:12:43",
@@ -110,19 +192,41 @@ test_aladin_string_target = [
     "42.67 +25.48",
     "42.67 -25.48",
     "0 0",
+    "J42.67 25.48",
+    "G42.67 25.48",
+    "B42.67 25.48",
+    "J12 30 45 +45 30 15",
+    "J03 15 20 -10 20 30",
+    "G120.5 -45.7",
+    "G90 0",
+    "B60 30",
+    "B120 -45",
 ]
 
 
 @pytest.mark.parametrize("target", test_aladin_string_target)
 def test_aladin_string_target_set(target):
+    """
+    Test setting the target of an Aladin object with a string or a SkyCoord object
+    :param target: The target string
+    :return: None
+    """
     aladin = Aladin()
     aladin.target = target
-    assert aladin.target == parse_coordinate_string(target)
+    parsed_target = parse_coordinate_string(target)
+    assert aladin.target.icrs.ra.deg == parsed_target.icrs.ra.deg
+    assert aladin.target.icrs.dec.deg == parsed_target.icrs.dec.deg
 
 
 @pytest.mark.parametrize("target", test_aladin_string_target)
 def test_aladin_sky_coord_target_set(target):
+    """
+    Test setting and getting the target of an Aladin object with a SkyCoord object
+    :param target: The target string
+    :return: None
+    """
     sc_target = parse_coordinate_string(target)
     aladin = Aladin()
     aladin.target = sc_target
-    assert aladin.target == sc_target
+    assert aladin.target.icrs.ra.deg == sc_target.icrs.ra.deg
+    assert aladin.target.icrs.dec.deg == sc_target.icrs.dec.deg
