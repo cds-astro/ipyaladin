6ee40001288b23d489cfaae628f1d19ca0a6c853
Celia Parts <cparts@stsci.edu>
2025-08-19 11:16:23 -0400

    initial updates from Matthieu, adding noqa to avoid ruff errors on PLC0415

--- js/models/message_handler.js
+++ js/models/message_handler.js
@@ -38,10 +38,6 @@ export default class MessageHandler {
     this.aladin.gotoRaDec(msg["ra"], msg["dec"]);
   }
 
-  handleChangeRotation(msg) {
-    this.aladin.setRotation(msg["rotation"]);
-  }
-
   async handleSaveViewAsImage(msg) {
     const path = msg["path"];
     const format = msg["format"];
